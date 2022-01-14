47212239e45a73cacd0ed112f2b35d9bbc3173bb
Leonid <Chunl33666@yandex.ru>
2022-01-14 22:56:47 +0300

    Переход на функциональные компоненты

--- src/components/cards-item-app/cards-item.js
+++ src/components/cards-item-app/cards-item.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class CardItem extends Component {
-    render() {
-        const { label, year, description, image } = this.props;
-        let classNames = "card grid shadow-md shadow-slate-200  hover:shadow-slate-400 border-2 rounded-md bg-white"
-        return (
-            <div className={classNames}>
-                <div className="card__img w-full">
-                    {image}
-                </div>
-                <div className="card__title py-4">
-                    <p className="text-center font-semibold text-2xl text-slate-400 ">{label}</p>
-                </div>
-                <div className="card__year">
-                    <p className="text-center font-semibold text-lg text-slate-400 p-0">{year}</p>
-                </div>
-                <div className="card__description text-center py-4 px-3 text-slate-500">
-                    <p>{description}</p>
-                </div>
+function CardItem(props) {
+    let classNames = "card grid shadow-md shadow-slate-200  hover:shadow-slate-400 border-2 rounded-md bg-white"
+    return (
+        <div className={classNames}>
+            <div className="card__img w-full">
+                {props.image}
             </div>
-        )
-    }
-
+            <div className="card__title py-4">
+                <p className="text-center font-semibold text-2xl text-slate-400 ">{props.label}</p>
+            </div>
+            <div className="card__year">
+                <p className="text-center font-semibold text-lg text-slate-400 p-0">{props.year}</p>
+            </div>
+            <div className="card__description text-center py-4 px-3 text-slate-500">
+                <p>{props.description}</p>
+            </div>
+        </div>
+    )
 }
+export default CardItem;
+
