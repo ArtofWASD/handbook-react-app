cbe2d0326b07afe0c28a4b9de3758aae2888dfd3
Leonid <Chunl3e666@yandex.ru>
2022-01-14 17:41:14 +0300

    Картинки на главную

--- src/components/cards-item-app/cards-item.js
+++ src/components/cards-item-app/cards-item.js
@@ -8,7 +8,6 @@ export default class CardItem extends Component {
             <div className={classNames}>
                 <div className="card__img w-full">
                     {image}
-                    <img src="https://images.unsplash.com/photo-1527786356703-4b100091cd2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="placeholder"></img>
                 </div>
                 <div className="card__title py-4">
                     <p className="text-center font-semibold text-2xl text-slate-400 ">{label}</p>
--- src/components/footer-app/footer-app.js
+++ src/components/footer-app/footer-app.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () =>{
     return (
         <div className="footer grid bg-blue-600 h-20  mt-4">
-            <p className="grid align-end footer__title text-center text-slate-300 text-sm">Designed by Chunlee</p>
+            <p className="grid align-item-end footer__title text-center text-slate-300 text-sm">Designed by Chunlee</p>
         </div>
     )
 }
