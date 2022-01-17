6d764e99e38d10eec6f2fe39d4787e5f02f8f0db
Leonid <Chunl33666@yandex.ru>
2022-01-18 00:52:48 +0300

    Сраный роутер

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,32 +1,31 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
 import Header from "../header-app/header-app"
 import SearchBar from "../search-app/search-app"
 import CarsList from "../cars-list-app/cars-list"
 import Footer from "../footer-app/footer-app"
 import Data from "../data/data"
-import PartsListGroup from "../parts-group-list-app/parts-group-list"
+//import Layout from '../layout-app/layout-app'
 
 import './app.css';
+
 
 
 export default function App() {
   return (
     <React.Fragment>
-
-        <Header />
-        <SearchBar />
-        <Routes>
-          <Route path="/PartsListGroup" element={<PartsListGroup />} />
-          <Route path="/" element={<CarsList
-            posts={Data}
-          />} />
-        </Routes>
-        <Footer />
+      <Header />
+      <SearchBar />
+      <Routes>
+        <Route path="*" element={<CarsList posts={Data} />} />
+      </Routes>
+      <Footer />
 
     </React.Fragment>
   )
 }
+
+
 
 
 
--- src/components/cars-item-app/cars-item.js
+++ src/components/cars-item-app/cars-item.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
 
 export default function CarsItem(props) {
     let classNames = "card grid shadow-md shadow-slate-200 hover:shadow-slate-400 border-2 rounded-md bg-white"
+
+
     return (
-        <div className={classNames}>
-            <div className="card__img w-full p-2">
-                {props.image}
+        <>
+            <div className={classNames} value={props.id} >
+                <div className="card__img w-full p-2">
+                    {props.image}
+                </div>
+                <div className="card__title py-4">
+                    <p className="text-center font-semibold text-2xl text-slate-400">{props.label}</p>
+                </div>
+                <div className="card__year">
+                    <p className="text-center font-semibold text-lg text-slate-400 p-0">{props.year}</p>
+                </div>
+                <div className="card__description text-center py-4 px-3 text-slate-500">
+                    <p>{props.description}</p>
+                </div>
+                <div>
+                    <Link to="/PartsListGroup"><p className='text-center text-sm text-slate-500 py-2'>Подробней</p></Link>
+                </div>
             </div>
-            <div className="card__title py-4">
-                <p className="text-center font-semibold text-2xl text-slate-400">{props.label}</p>
-            </div>
-            <div className="card__year">
-                <p className="text-center font-semibold text-lg text-slate-400 p-0">{props.year}</p>
-            </div>
-            <div className="card__description text-center py-4 px-3 text-slate-500">
-                <p>{props.description}</p>
-            </div>
-            <div>
-                <Link to='../PartsListGroup'><p className='text-center text-sm text-slate-500 py-2'>Подробней</p></Link>
-            </div>
-        </div>
+        </>
+
     )
 }
-
