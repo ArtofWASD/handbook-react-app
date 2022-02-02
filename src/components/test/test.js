9e6d19469b4aac4dba986e123d319f86f035e49d
Leonid <Chunl3e666@yandex.ru>
2022-02-02 17:46:29 +0300

    Борьба с фетчем

--- src/components/test/test.js
+++ src/components/test/test.js
@@ -1,46 +1,36 @@
 import React, { useState, useEffect } from 'react';
 const Test = () => {
-
-    const [cars, setCars] = useState([])
-
-    // useEffect(() => {
-    //     fetch('http://localhost:3001/models')
-    //         .then(result => result.json())
-    //         .then(data => setCars(data))
-    // }, [])
-
-
-    function getData() {
-        // Запрос на сервер
-        fetch('http://localhost:3001/models')
-        // обрабатываем промис и получаем json
-            .then(result => result.json())
-            // передаём json в функцию
-            .then(data => carList(data))
-        
-    }
-    getData();
-
-    // Создаём функцию которая на входе принимает массив обработанный в getDataS
+    const [cars, setCars] = useState(undefined)
+    useEffect(()=>{
+        const getData = () => {
+            fetch('http://localhost:3001/models')
+                .then(result => result.json())
+                .then(data => carList(data))
+        }
+            getData();        
+    }, [])
     const carList = (data) => {
-        // находим элемент в массиве по id
         const result = new Promise((resolve, reject) => {
             const res = data.find(item => item.id === 1)
             resolve(res)
         })
-        .then(res => console.log(res))
+        .then(res => setCars(res))
     }
-
+    const result = (cars) ? cars.cars.find(item => item.id == 11) : console.log("w8 for cars");
     return (
         <div className= "text-center py-5">
             {cars && (
                 <div>
                     id this page is {cars.id}
+                    {result &&(
+                        <>
+                        {result.id}
+                        </>
+                    )}
                 </div>
             )}
         </div>
     );
 }
-
 export default Test;
 
