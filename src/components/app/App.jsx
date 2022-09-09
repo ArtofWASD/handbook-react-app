bfa0b6506cfdb26dd626111adcddb105b922e123
Leonid <Chunl3e666@yandex.ru>
2022-09-09 15:29:40 +0300

    fix: some bugs

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom';
-import { fetchData } from '../../services/reducers/data-slice';
-import Layout from '../layout/layout';
+import Layout from '../layout/Layout';
 import Homepage from '../homepage/homepage';
 import CarsList from '../cars-list/cars-list';
 import CarsPartsList from '../cars-parts-list/cars-parts-list';
