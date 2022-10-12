c793ed8e0ba833b9dfa7137f39cfff03fb015a22
Leonid <Chunl3e666@yandex.ru>
2022-10-12 16:54:23 +0300

    fix: more types for god of Types

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,13 +2,15 @@ import { FC } from "react";
 type TButton = { title: string; onClickHandler: () => void };
 const Button: FC<TButton> = ({ title, onClickHandler }) => {
   return (
-    <button
-      className="w-40 bg-blue-500 hover:bg-white text-white font-semibold hover:text-blue-700 p-2 border hover:border-blue-500 rounded"
-      onClick={onClickHandler}
-      title={title}
-    >
-      {title}
-    </button>
+    <>
+      <button
+        className="w-40 bg-blue-500 hover:bg-white text-white font-semibold hover:text-blue-700 p-2 border hover:border-blue-500 rounded"
+        onClick={onClickHandler}
+        title={title}
+      >
+        {title}
+      </button>
+    </>
   );
 };
 export default Button;
