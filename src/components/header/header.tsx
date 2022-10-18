d10b460194950fc93e4db274996037c51b23a77d
Leonid <Chunl33666@yandex.ru>
2022-10-18 15:06:34 +0300

    замена инпута в шапке

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,11 @@ import Button from "../../ui/button/button";
 import Breadcrumb from "../breadcrumbs/breadcrumbs";
 import { Link, useLocation } from "react-router-dom";
 import { useAppSelector } from "utils/hooks";
+import Input from "ui/input/input";
+import { useState } from "react";
 
 const Header = () => {
+  const [searchValue, setSearchValue] = useState("");
   const location = useLocation();
   const isUserLogin = useAppSelector((state) => state.user.isLogin);
 
@@ -11,17 +14,13 @@ const Header = () => {
     <header className="header grid grid-cols-auto ">
       <div className="header__button grid justify-self-end  py-4 px-4">
         {isUserLogin ? (
-          <>
-            <Link to="account">
-              <Button title="Личный кабинет" className="w-48" />
-            </Link>
-          </>
+          <Link to="account">
+            <Button title="Личный кабинет" className="w-48" />
+          </Link>
         ) : (
-          <>
-            <Link to="login">
-              <Button title="Авторизация" className="w-48" />
-            </Link>
-          </>
+          <Link to="login">
+            <Button title="Авторизация" className="w-48" />
+          </Link>
         )}
       </div>
       <div className="header__title grid justify-self-center py-4">
@@ -30,10 +29,12 @@ const Header = () => {
         </p>
       </div>
       <div className="header__search grid container lg:grid-cols-6 sm:grid-cols-1  justify-self-center px-10 gap-2">
-        <input
+        <Input
           type="text"
-          className="lg:col-start-2 lg:col-span-3 border border-blue-500 rounded px-4 py-2 font-semibold"
+          className="lg:col-start-2 lg:col-span-3 border border-blue-500 px-4 py-2 font-semibold"
           placeholder="Номер запчасти или название"
+          value={searchValue}
+          onChangeHandler={(e) => setSearchValue(e.target.value)}
         />
         <Button title="Поиск" className="w-48"></Button>
       </div>
