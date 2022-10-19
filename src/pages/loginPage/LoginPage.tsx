23e071bf6ffff056b66737e2ece078b96edc2b77
Leonid <Chunl3e666@yandex.ru>
2022-10-19 17:25:27 +0300

    Изменение страниц регистрации и иавторизации

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,42 +1,78 @@
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { setUser } from "services/reducers/userSlice";
-import Form from "ui/form/Form";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Input from "ui/input/input";
+import { useState } from "react";
+import Button from "ui/button/button";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const loginHandler = (email: string, password: string) => {
     const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
       .then(({ user }) => {
+        console.log(user);
         dispatch(
           setUser({
             email: user.email,
             id: user.uid,
             token: user.refreshToken,
-          })
+          }),
         );
-        navigate("/");
       })
       .catch(console.error);
   };
 
   return (
     <>
-      <Form
-        title="Введите ваш логин и пароль"
-        buttonTitle="Войти"
-        onClickHandler={loginHandler}
-      />
-      <span className="flex justify-center text-xl font-semibold">
-        Нет учётной записи ?{" "}
-        <Link to="/register" className="text-blue-700 px-2">
-          Регистрация
-        </Link>
-      </span>
+      <form
+        action="submit"
+        onSubmit={(e) => {
+          e.preventDefault();
+          loginHandler(email, password);
+          navigate("/");
+        }}
+        className=" flex flex-col items-center"
+      >
+        <Input
+          type="email"
+          placeholder="Почта"
+          value={email}
+          onChangeHandler={(e) => setEmail(e.target.value)}
+          className=" w-80 my-2"
+        />
+        <Input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChangeHandler={(e) => setPassword(e.target.value)}
+          className=" w-80 my-2"
+        />
+        <Button
+          title="Войти"
+          onClickHandler={() => loginHandler(email, password)}
+          className="w-36 my-2"
+        ></Button>
+      </form>
+      <div className="flex flex-col items-center text-xl font-semibold py-4">
+        <span>
+          Нет учётной записи ?
+          <Link to="/register" className="text-blue-700 px-2">
+            Зарегестрируйтесь
+          </Link>
+        </span>
+        <span>
+          Вернуться на
+          <Link to="/" className="text-blue-700 px-2">
+            главную
+          </Link>
+        </span>
+      </div>
     </>
   );
 };
--- src/pages/registerPage/RegisterPage.tsx
+++ src/pages/registerPage/RegisterPage.tsx
@@ -1,20 +1,26 @@
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { setUser } from "services/reducers/userSlice";
-import Form from "ui/form/Form";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import Input from "ui/input/input";
+import Button from "ui/button/button";
+import { useState } from "react";
 
 const Register = () => {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail ] = useState("");
+  const [password, setPassword ] = useState("");
+  
 
-  const registerHandler = (email:string, password:string) => {
+  const registerHandler = (email: string, password: string, name:string) => {
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
       .then(({ user }) => {
         dispatch(
           setUser({
+            name:user.displayName,
             email: user.email,
             id: user.uid,
             token: user.refreshToken,
@@ -25,11 +31,52 @@ const Register = () => {
       .catch(console.error);
   };
   return (
-    <Form
-      title="Регистрация нового пользователя"
-      onClickHandler={registerHandler}
-      buttonTitle="Регистрация"
-    />
+    <>
+      <form
+        action="submit"
+        onSubmit={(e) => {
+          e.preventDefault();
+          registerHandler(email, password, name);
+          navigate("/");
+        }}
+        className=" flex flex-col items-center"
+      >
+        <Input
+          type="name"
+          placeholder="Ваше имя"
+          value={email}
+          onChangeHandler={(e) => setName(e.target.value)}
+          className=" w-80 my-2"
+        />
+        <Input
+          type="email"
+          placeholder="Почта"
+          value={email}
+          onChangeHandler={(e) => setEmail(e.target.value)}
+          className=" w-80 my-2"
+        />
+        <Input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChangeHandler={(e) => setPassword(e.target.value)}
+          className=" w-80 my-2"
+        />
+        <Button
+          title="Зарегестрироваться"
+          onClickHandler={() => registerHandler(email, password, name)}
+          className="w-48 my-2"
+        ></Button>
+      </form>
+      <div className="flex justify-center text-xl font-semibold py-4">
+        <span>
+          Вернуться на
+          <Link to="/" className="text-blue-700 px-2">
+            главную
+          </Link>
+        </span>
+      </div>
+    </>
   );
 };
 
