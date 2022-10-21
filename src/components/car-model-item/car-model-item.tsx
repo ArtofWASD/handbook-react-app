dbc46b4dbc9b066bb707092c616f1ce802467ecb
Leonid <Chunl3e666@yandex.ru>
2022-10-21 17:41:33 +0300

    акутальный год

--- src/components/car-model-item/car-model-item.tsx
+++ src/components/car-model-item/car-model-item.tsx
@@ -11,6 +11,7 @@ type TCarModelItem = {
   route: string;
 };
 const CarModelItem: FC<TCarModelItem> = ({ data, route }) => {
+  const year = new Date()
   return (
     <div className="card grid shadow-md shadow-slate-200 hover:border-blue-500 border-2 rounded-md bg-white max-w-xs">
       <div className="card__img p-2">
@@ -23,7 +24,7 @@ const CarModelItem: FC<TCarModelItem> = ({ data, route }) => {
       </div>
       <div className="card__year py-4">
         <p className="text-center font-semibold text-lg text-slate-400 p-0">
-          {data.year}
+          {data.year} - {year.getFullYear()}
         </p>
       </div>
       <div className="card__link">
