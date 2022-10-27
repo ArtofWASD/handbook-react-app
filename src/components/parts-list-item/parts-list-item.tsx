cccca73912beac22471c8688316361f0a520f47a
Leonid <Chunl3e666@yandex.ru>
2022-10-27 17:39:26 +0300

    название категории в карточке группы

--- src/components/parts-list-item/parts-list-item.tsx
+++ src/components/parts-list-item/parts-list-item.tsx
@@ -14,8 +14,11 @@ const PartsListItem: FC<TPartsListItem> = ({ data, route }) => {
       className="parts__item border-2 rounded-xl shadow-md shadow-slate-200 hover:border-blue-500 bg-white lg:w-48 mx-1 p-2"
       key={data.id}
     >
-      <div className="parts__item-image">
+      <div className="parts__item-image flex justify-center">
         <img src={data.image} alt={data.title} />
+      </div>
+      <div className="parts__item-title text-center text-md pt-2 font-semibold text-slate-500">
+        <p>{data.title}</p>
       </div>
       <div className="parts__link text-center">
         <Link key={data.id} to={route}></Link>
