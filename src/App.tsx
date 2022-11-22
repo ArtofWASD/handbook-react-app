e7b176783fdfbb0e2cc8e454d1ed54cca8d0291f
Leonid <Chunl3e666@yandex.ru>
2022-11-22 12:59:45 +0300

    env ебля

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,35 @@ import LoginPage from "../src/pages/login-page/login_page";
 import AccountPage from "../src/pages/account-page/account-page";
 import PartPostPage from "../src/components/part-post-page/part-post-page";
 import { useAppDispatch, useAppSelector } from "../src/utils/hooks";
+import { useEffect } from "react";
+import { supabase } from "./utils/supabase";
 
 export default function App() {
   const dispatch = useAppDispatch();
+  useEffect(() => {
+    const getCars = async () => {
+      const { data: cars, error } = await supabase.from("cars").select("*");
+      console.log(cars);
+      
+    };
+    getCars();
+  }, []);
 
   return (
     <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="/:id" element={<ChildrenCarsList />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="account" element={<AccountPage />} />
-          <Route path="/:id/:childId" element={<CarsPartsList />} />
-          <Route path="/:id/:childId/:partId" element={<PartPostList />} />
-          <Route
-            path="/:id/:childId/:partId/:postId"
-            element={<PartPostPage />}
-          />
-        </Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="/:id" element={<ChildrenCarsList />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="account" element={<AccountPage />} />
+        <Route path="/:id/:childId" element={<CarsPartsList />} />
+        <Route path="/:id/:childId/:partId" element={<PartPostList />} />
+        <Route
+          path="/:id/:childId/:partId/:postId"
+          element={<PartPostPage />}
+        />
+      </Route>
     </Routes>
   );
 }
