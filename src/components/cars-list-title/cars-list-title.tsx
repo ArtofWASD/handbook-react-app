c36381afea2a763c8d733c75ea3c42defad3c002
Leonid <Chunl3e666@yandex.ru>
2022-11-23 17:53:13 +0300

    still working

--- src/components/cars-list-title/cars-list-title.tsx
+++ src/components/cars-list-title/cars-list-title.tsx
@@ -3,8 +3,8 @@ import { year } from "../../utils/handlerFunctions";
 
 type TCarsListTitle = {
   data: {
-    image: string;
-    label: string;
+    imgUrl: string;
+    name: string;
     year: string;
   };
 };
@@ -13,11 +13,11 @@ const CarsListTitle: FC<TCarsListTitle> = ({ data }) => {
   return (
     <div className="cars_list_title border-2 rounded-xl grid lg:grid-cols-2 sm:grid-cols-1 justify-self-center my-2 bg-white">
       <div className="cars__image grid justify-items-center lg:w-80 px-2 ">
-        <img src={data.image} alt={data.label} />
+        <img src={data.imgUrl} alt={data.name} />
       </div>
       <div className="cars__description grid content-center px-4">
         <div className="text-4xl text-center font-semibold py-2 text-slate-500">
-          {data.label}
+          {data.name}
         </div>
         <div className="text-4xl text-center font-semibold py-2 text-slate-500">
           {data?.year?.length > 4 ? data.year : `${data.year} - ${year}`}
