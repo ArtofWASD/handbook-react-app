de417078e26d93d499f60e9275a329bcb9b7a7ef
Leonid <Chunl3e666@yandex.ru>
2022-11-24 17:39:19 +0300

    fix parts list page

--- src/pages/cars-parts-list-page/cars-parts-list-page.tsx
+++ src/pages/cars-parts-list-page/cars-parts-list-page.tsx
@@ -5,34 +5,42 @@ import PartsListItem from "../../components/parts-list-item/parts-list-item";
 import { useAppSelector } from "../../utils/hooks";
 
 const CarsPartsList = () => {
-  const { childId, id } = useParams();
+  const { childName, name } = useParams();
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
-  const carsData = useAppSelector((state) => state.data.data);
-  
-  const childCarArray = carsData
-    .find((item: { id: string }) => item.id === String(id))
-    ?.childCars.find((item: { id: string }) => item.id === String(childId));
 
+  const carsData: any = useAppSelector((state) => state.data.data);
+  const childCarArray: any = carsData
+    .find((item: { name: string }) => item.name === String(name))
+    ?.childCars.find(
+      (item: { name: string }) => item.name === String(childName),
+  );
+  
   return (
-    <div className="cars_parts_list_page grid justify-center">
-      {childCarArray && <СarsListTitle data={childCarArray} />}
-      <div className="parts__group-list grid lg:grid-cols-4 sm:grid-cols-2 gap-4 py-4 px-1">
-        {childCarArray?.parts.map((item: any) => (
-          <PartsListItem
-            data={item}
-            route={`/${id}/${childId}/${item.id}`}
-            key={item.id}
-          />
-        ))}
-      </div>
-      <div className="nav grid lg:grid-cols-[20%_20%] md:grid-cols-[40%_40%] grid-cols-2 py-4 justify-end gap-2 px-2">
-        <Link to="../">
-          <Button title="На главную" className="w-36" />
-        </Link>
-        <Button title="Назад" onClickHandler={goBack} className="w-36" />
-      </div>
-    </div>
+    <>
+      {childCarArray !== undefined ? (
+        <div className="cars_parts_list_page grid justify-center">
+          {childCarArray && <СarsListTitle data={childCarArray} />}
+          <div className="parts__group-list grid lg:grid-cols-4 sm:grid-cols-2 gap-4 py-4 px-1">
+            {childCarArray?.partsGroup.map((item: any) => 
+              <PartsListItem
+                data={item.part}
+                route={`/${name}/${childName}/${item.part.name}`}
+                key={item.part.id}
+              />
+            )}
+          </div>
+          <div className="nav grid lg:grid-cols-[20%_20%] md:grid-cols-[40%_40%] grid-cols-2 py-4 justify-end gap-2 px-2">
+            <Link to="../">
+              <Button title="На главную" className="w-36" />
+            </Link>
+            <Button title="Назад" onClickHandler={goBack} className="w-36" />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 export default CarsPartsList;
