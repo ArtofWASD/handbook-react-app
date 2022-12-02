40e5c99f51dd058a4b3340cf316f105f319ff923
Leonid <Chunl3e666@yandex.ru>
2022-12-02 17:05:06 +0300

    фикс вёрстки

--- src/components/cars-list-title/cars-list-title.tsx
+++ src/components/cars-list-title/cars-list-title.tsx
@@ -11,15 +11,15 @@ type TCarsListTitle = {
 // Заголовок в старницах с машинами
 const CarsListTitle: FC<TCarsListTitle> = ({ data }) => {
   return (
-    <div className="cars_list_title border-2 rounded-xl grid lg:grid-cols-2 sm:grid-cols-1 justify-self-center my-2 bg-white">
-      <div className="cars__image grid justify-items-center lg:w-80 px-2 ">
+    <div className="cars_list_title border-2 rounded-xl grid grid-cols-2 justify-self-center my-2 px-7 bg-white">
+      <div className="cars__image grid justify-items-center lg:w-80 w-32 md:w-48 px-2 ">
         <img src={data.imgUrl} alt={data.name} />
       </div>
       <div className="cars__description grid content-center px-4">
-        <div className="text-4xl text-center font-semibold py-2 text-slate-500">
+        <div className="text-lg md:text-4xl text-center font-semibold py-0 lg:py-2 text-slate-500">
           {data.name}
         </div>
-        <div className="text-4xl text-center font-semibold py-2 text-slate-500">
+        <div className="text-lg md:text-4xl text-center font-semibold py-0 lg:py-2 text-slate-500">
           {data?.year?.length > 4 ? data.year : `${data.year} - ${year}`}
         </div>
       </div>
