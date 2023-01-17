d4b6ac24cf65c95d2b29b6ebebe81ac0df4dbc19
Leonid <Chunl3e666@yandex.ru>
2023-01-17 09:25:58 +0300

    Удаляем тестовые данные

--- src/components/car-model-info/car-model-info.tsx
+++ src/components/car-model-info/car-model-info.tsx
@@ -1,5 +1,5 @@
 import { Popover } from "@headlessui/react";
-
+// Поповер отображающий характеристики выбранного автомобиля
 const carModelInfo = () => {
   return (
     <Popover className="grid justify-center">
@@ -11,43 +11,31 @@ const carModelInfo = () => {
           <div className="">
             <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">Двигатель</p>
             <ul className="">
-              <li className="font-semibold text-slate-500">Бензин 2.0</li>
-              <li className="font-semibold text-slate-500">Дизель 2.5 DI</li>
-              <li className="font-semibold text-slate-500">Дизель 2.5 TD</li>
+
             </ul>
           </div>
           <div className="">
             <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">Коробка передач</p>
             <ul className="">
-              <li className="font-semibold text-slate-500">Механическая </li>
             </ul>
           </div>
           <div className="">
             <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">Мост</p>
             <ul className="">
-              <li className="font-semibold text-slate-500">Type 32</li>
-              <li className="font-semibold text-slate-500">Type 35</li>
+
             </ul>
           </div>
           <div className="">
             <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">Технические жидкости</p>
             <ul className="">
-              <li className="font-semibold text-slate-500">Масло моторное 5W-30</li>
-              <li className="font-semibold text-slate-500">Масло моторное 5W-30</li>
+
             </ul>
           </div>
           <div className="">
             <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">Серии</p>
             <ul className="">
-              <li className="font-semibold text-slate-500">80</li>
-              <li className="font-semibold text-slate-500">120</li>
-              <li className="font-semibold text-slate-500">150</li>
-              <li className="font-semibold text-slate-500">190</li>
-              <li className="font-semibold text-slate-500">100L</li>
+
             </ul>
-          </div>
-          <div className="">
-            <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">Ещё не придумал</p>
           </div>
         </div>
       </Popover.Panel>
