82523b155d0ece7b4dc642aa35ea2f7426574110
Leonid <chunlee666@gmail.com>
2023-01-19 21:54:51 +0300

    Анимация открытия поповера

--- src/components/car-model-info/car-model-info.tsx
+++ src/components/car-model-info/car-model-info.tsx
@@ -1,4 +1,4 @@
-import { Popover } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 import { useState } from "react";
 import CarModelInfoDetails from "../car-model-info-details/car-model-info-details";
 import Modal from "../modal/Modal";
@@ -9,75 +9,84 @@ type TCarModelInfo = {
 // Поповер отображающий характеристики выбранного автомобиля
 const carModelInfo = (data: any) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  
+
   return (
     <>
       <Popover className="grid">
         <Popover.Button className="text-xl font-semibold text-slate-500 py-4">
           Характеристки автомобиля
         </Popover.Button>
-        <Popover.Panel>
-          {({ close }) => (
-            <div className="lg:grid grid-flow-col lg:px-0 mx-4 ">
-              <div className="">
-                <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
-                  Двигатель
-                </p>
-                <ul className="">
-                  {data?.data.engines.map((item: any) => (
-                    <li
-                      key={item.engine.id}
-                      className="font-semibold text-slate-500 hover:cursor-pointer hover:underline underline-offset-4 decoration-blue-500"
-                      onClick={() => {
-                        setIsModalOpen(true);
-                        close();
-                      }}
-                    >
-                      {item.engine.type} {item.engine.name}
-                    </li>
-                  ))}
-                </ul>
+        <Transition
+          enter="transition duration-500 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-250 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <Popover.Panel>
+            {({ close }) => (
+              <div className="lg:grid grid-flow-col lg:px-0 mx-4 ">
+                <div className="">
+                  <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
+                    Двигатель
+                  </p>
+                  <ul className="">
+                    {data?.data.engines.map((item: any) => (
+                      <li
+                        key={item.engine.id}
+                        className="font-semibold text-slate-500 hover:cursor-pointer hover:underline underline-offset-4 decoration-blue-500"
+                        onClick={() => {
+                          setIsModalOpen(true);
+                          close();
+                        }}
+                      >
+                        {item.engine.type} {item.engine.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="">
+                  <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
+                    Коробка передач
+                  </p>
+                  <ul className="">
+                    {data?.data.gearboxes.map((item: any) => (
+                      <li
+                        key={item.gearbox.id}
+                        className="font-semibold text-slate-500 hover:cursor-pointer hover:underline underline-offset-4 decoration-blue-500"
+                        onClick={() => {
+                          setIsModalOpen(true);
+                          close();
+                        }}
+                      >
+                        {item.gearbox.name} ({item.gearbox.type})
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="">
+                  <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
+                    Мост
+                  </p>
+                  <ul className=""></ul>
+                </div>
+                <div className="">
+                  <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
+                    Технические жидкости
+                  </p>
+                  <ul className=""></ul>
+                </div>
+                <div className="">
+                  <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
+                    Серии
+                  </p>
+                  <ul className=""></ul>
+                </div>
               </div>
-              <div className="">
-                <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
-                  Коробка передач
-                </p>
-                <ul className="">
-                {data?.data.gearboxes.map((item: any) => (
-                    <li
-                      key={item.gearbox.id}
-                      className="font-semibold text-slate-500 hover:cursor-pointer hover:underline underline-offset-4 decoration-blue-500"
-                      onClick={() => {
-                        setIsModalOpen(true);
-                        close();
-                      }}
-                    >
-                      {item.gearbox.name} ({item.gearbox.type})
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="">
-                <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
-                  Мост
-                </p>
-                <ul className=""></ul>
-              </div>
-              <div className="">
-                <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
-                  Технические жидкости
-                </p>
-                <ul className=""></ul>
-              </div>
-              <div className="">
-                <p className="font-semibold text-slate-500 underline underline-offset-4 decoration-2 pb-2">
-                  Серии
-                </p>
-                <ul className=""></ul>
-              </div>
-            </div>
-          )}
-        </Popover.Panel>
+            )}
+          </Popover.Panel>
+        </Transition>
       </Popover>
       <Modal
         isOpen={isModalOpen}
