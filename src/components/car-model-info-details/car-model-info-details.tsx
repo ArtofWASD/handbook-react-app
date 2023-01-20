22efecfdb85764c9e5b6fc44f39f6a69b6eb0fab
Leonid <Chunl3e666@yandex.ru>
2023-01-20 17:24:41 +0300

    Вёрска + выборка по воздушным фильтрам.

--- src/components/car-model-info-details/car-model-info-details.tsx
+++ src/components/car-model-info-details/car-model-info-details.tsx
@@ -1,20 +1,21 @@
 import { useAppSelector } from "../../utils/hooks";
 
-// Отображение детальной информации о интересующем узле автомобиля из информаци о машине
+// Отображение детальной информации о интересующем узле автомобиля из информаци о двигателе
 const CarModelInfoDetails = () => {
-  const data: any = useAppSelector((state) => state.data.engineInfo);
+  const data = useAppSelector((state) => state.data.engineInfo);
   return (
     <>
       {data !== undefined && data !== null ? (
-        <div className="py-5 px-2">
-          <div className="">
-            Тут пока ничего нету, без паники, это временно!
+        <div className="py-3 px-2">
+          <div className="text-center text-xl font-semibold text-slate-500">
+            Подробная информация:
           </div>
           {data.map((item: any) => (
             <div key={item.id} className="flex-col gap-2">
-              <div>Cпецификация масла: {item.engineOilSpecification}</div>
-              <div>Объём масла: {item.engineOilAmount}</div>
-              <div>Масляные фильтры: {item.oilFilters}</div>
+              <div className=" text-slate-500">- Cпецификация масла: {item.engineOilSpecification}</div>
+              <div className=" text-slate-500">- Объём масла: {item.engineOilAmount}</div>
+              <div className=" text-slate-500">- Масляные фильтры: {item.oilFilters}</div>
+              <div className=" text-slate-500">- Воздушные фильтры: {item.airFilters}</div>
             </div>
           ))}
         </div>
